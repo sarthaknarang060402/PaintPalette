8133037d24a92263a6b8e77f4a0b5428220d5990
Sarthak Narang <91564487+sarthaknarang060402@users.noreply.github.com>
2023-08-03 01:44:22 +0530

    Update jscolor.js

--- jscolor.js
+++ jscolor.js
@@ -1,14 +1,3 @@
-/**
- * jscolor - JavaScript Color Picker
- *
- * @link    http://jscolor.com
- * @license For open source use: GPLv3
- *          For commercial use: JSColor Commercial License
- * @author  Jan Odvarko
- * @version 2.0.5
- *
- * See usage examples at http://jscolor.com/examples/
- */
 
 
 "use strict";
